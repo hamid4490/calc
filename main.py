71d237637138375275e2bd06fa7bf5e40c858252
hamid4490 <31180592+hamid4490@users.noreply.github.com>
2025-06-26 02:59:52 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from kivy.app import App
 from kivy.uix.label import Label
 class Test(App):
     def build(self):
-        return Label(text="Hello")
+        return Label(text="Hello parham")
 Test().run()
